985c90afd41c8e7fc9cb16f3935bef6aff5cdc4e
michael <renzettimichael1@gmail.com>
2020-08-27 20:00:47 -0400

    updating example 3 a bit, example 2 still does not work. May need to get help on this one

--- src/examples/threeLinkExample.cpp
+++ src/examples/threeLinkExample.cpp
@@ -31,25 +31,25 @@ int main(int argc, char* argv[])
   robot::Filter<robot::sharedThreeLinkRobot> filter = controller.filter();
 
   // To run the controller all the user needs to do is populate the robot with measured and desired states and then execute the controller //
-  robot->theta(0) = 0.3021f; // measured rad
-  robot->theta(1) = -0.7063f; // measured rad
-  robot->theta(2) = 0.7140f; // measured rad
+  robot->theta(0) = 0.0028f; // measured rad
+  robot->theta(1) = 0.0012f; // measured rad
+  robot->theta(2) = -0.0097f; // measured rad
 
-  robot->dtheta(0) = -0.3404f; // measured rad/sec
-  robot->dtheta(1) = 0.8760f; // measured rad/sec
-  robot->dtheta(2) = -1.0325f; // measured rad/sec
+  robot->dtheta(0) = 0.4452e-3f; // measured rad/sec
+  robot->dtheta(1) = -0.3447e-3f; // measured rad/sec
+  robot->dtheta(2) = -0.3264e-3f; // measured rad/sec
 
-  robot->theta_d(0) = 0.3438f; // desired rad
-  robot->dtheta_d(0) = -0.6876f; // desired rad/sec
-  robot->ddtheta_d(0) = 0.6876f; // desired rad/sec^2
+  robot->theta_d(0) = 0.3927f; // desired rad
+  robot->dtheta_d(0) = -0.7854f; // desired rad/sec
+  robot->ddtheta_d(0) = 0.7854f; // desired rad/sec^2
 
-  robot->theta_d(1) = -0.4718f; // desired rad
-  robot->dtheta_d(1) = 0.9436f; // desired rad/sec
-  robot->ddtheta_d(1) = -0.9436f; // desired rad/sec^2
+  robot->theta_d(1) = 0.0f; // desired rad
+  robot->dtheta_d(1) = 0.0f; // desired rad/sec
+  robot->ddtheta_d(1) = 0.0f; // desired rad/sec^2
 
-  robot->theta_d(2) = -1.8057f; // desired rad
-  robot->dtheta_d(2) = 3.6115f; // desired rad/sec
-  robot->ddtheta_d(2) = -3.6115f; // desired rad/sec^2
+  robot->theta_d(2) = -2.8125f; // desired rad
+  robot->dtheta_d(2) = 5.6250f; // desired rad/sec
+  robot->ddtheta_d(2) = -5.6250f; // desired rad/sec^2
   
   auto t1 = std::chrono::high_resolution_clock::now();
   controller.execute(robot);
